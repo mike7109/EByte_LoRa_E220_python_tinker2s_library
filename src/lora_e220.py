57581d2d3e9cf5dee101af7776294918fb580537
misha <misha.bah21@yandex.ru>
2024-11-13 19:34:20 +0300

    fix: fix

--- src/lora_e220.py
+++ src/lora_e220.py
@@ -1,3 +1,40 @@
+#############################################################################################
+# EBYTE LoRa E220 Series for Raspberry Pi
+#
+# AUTHOR:  Renzo Mischianti
+# VERSION: 0.0.2
+#
+# This library is based on the work of:
+# https://www.mischianti.org/category/my-libraries/lora-e220-devices/
+#
+# This library implements the EBYTE LoRa E220 Series for Raspberry Pi.
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2019 Renzo Mischianti www.mischianti.org All right reserved.
+#
+# You may copy, alter and reuse this code in any way you like, but please leave
+# reference to www.mischianti.org in your comments if you redistribute this code.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#############################################################################################
+
 from lora_e220_constants import UARTParity, UARTBaudRate, TransmissionPower, FixedTransmission, AirDataRate, \
     OperatingFrequency, LbtEnableByte, WorPeriod, RssiEnableByte, RssiAmbientNoiseEnable, SubPacketSetting
 from lora_e220_operation_constant import ResponseStatusCode, ModeType, ProgramCommand, SerialUARTBaudRate, \
@@ -327,7 +364,7 @@ class LoRaE220:
             self.m1_pin.write(False)  # Начальное состояние LOW
 
         code = self.set_mode(ModeType.MODE_0_NORMAL)
-        if code != ResponseStatusCode.E220_SUCCESS:
+        if code != ResponseStatusCode.SUCCESS:
             return code
 
         return code
