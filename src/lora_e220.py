02063b8d6cd9b9837c877775e2b6a555f776a6af
misha <misha.bah21@yandex.ru>
2024-11-13 19:18:50 +0300

    fix: fix

--- src/lora_e220.py
+++ src/lora_e220.py
@@ -201,7 +201,8 @@ def main():
     m1_pin = 27  # Замените на ваш пин M1
 
     # Создаем объект LoRaE220
-    lora = LoRaE220('400T22D', uart, aux_pin=aux_pin, m0_pin=m0_pin, m1_pin=m1_pin)
+    # lora = LoRaE220('400T22D', uart, aux_pin=aux_pin, m0_pin=m0_pin, m1_pin=m1_pin)
+    lora = LoRaE220('400T22D', uart)
 
     # Инициализируем модуль
     code = lora.begin()
